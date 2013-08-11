610df3b2259aa7e6be26b0135688fa25bed71b10
Rostyslav Bryzgunov <kottenator@gmail.com>
2013-08-11 16:04:08 +0300

    Launcher actions enabled

--- Grooveshark.user.js
+++ Grooveshark.user.js
@@ -5,61 +5,159 @@
 // @author         Rostyslav Bryzgunov <kottenator@gmail.com>
 // @require        utils.js
 // ==/UserScript==
-;(function() {
-    var Unity = external.getUnityObject(1.0);
-    var Grooveshark, DB;
-    var INIT_TIMEOUT = 15000;
+function _initGroovesharkWebApp() {
+    var GroovesharkIntegration = {
+        Unity: null,
 
-    Unity.init({
-        name: 'Grooveshark',
-        iconUrl: 'icon://grooveshark-logo',
-        onInit: wrapCallback(function() {
-            Unity.addAction('/About', newTab('http://github.com/kottenator/unity-grooveshark-webapp'));
-            Unity.Launcher.addAction("Loading player ...", function() {});
+        WEBAPP_TITLE: 'Grooveshark WebApp',
+        INIT_TIMEOUT: 10000,
 
-            // waiting for player to load (I haven't found better way than polling for maximum 15 sec)
-            var startDate = new Date();
-            var _t = setInterval(wrapCallback(function() {
-                if (unsafeWindow.Grooveshark) {
-                    clearInterval(_t);
+        // different actions
+        LOADING_PLAYER_ACTION: "Loading player ...",
+        LOADING_SONG_ACTION: "Loading song ...",
+        PLAY_ACTION: "▶ Play",
+        PAUSE_ACTION: "■ Pause",
+        NEXT_SONG_ACTION: "» Next song",
+        PREVIOUS_SONG_ACTION: "« Previous song",
+        COLLECT_SONG_ACTION: "✔ Collect song",
+        FAVOURITE_SONG_ACTION: "❤ Favourite song",
+        DEQUEUE_SONG_ACTION: "✕ Dequeue song",
 
-                    Grooveshark = unsafeWindow.Grooveshark;
-                    DB = unsafeWindow.GS.Models;
+        init: function() {
+            var Unity = this.Unity = external.getUnityObject(1.0);
+            var self = this;
 
-                    Unity.Launcher.removeActions();
-                    Unity.Launcher.addAction("Play", wrapCallback(play));
-                    Unity.Launcher.addAction("Pause", wrapCallback(pause));
-                    Unity.Launcher.addAction("Next song", wrapCallback(next));
-                    Unity.Launcher.addAction("Prev song", wrapCallback(previous));
-                } else if (new Date() - startDate > INIT_TIMEOUT) {
-                    clearInterval(_t);
+            Unity.init({
+                name: self.WEBAPP_TITLE,
+                iconUrl: 'icon://grooveshark-logo',
+                onInit: function() {
+                    // initial functionality
+                    Unity.addAction('/About', self.newTab('http://github.com/kottenator/unity-grooveshark-webapp'));
+                    Unity.Launcher.addAction(self.LOADING_PLAYER_ACTION, function() {
+                    });
 
-                    Unity.Launcher.removeActions();
-                    Unity.Notification.showNotification("Grooveshark", "Failed to init, sorry :(", null);
+                    // complete functionality
+                    // waiting for player to load (I haven't found better way than polling for maximum 30 sec)
+                    var startDate = new Date();
+                    var _t = setInterval(function() {
+                        if (window.Grooveshark) {
+                            clearInterval(_t);
+                            Unity.Launcher.removeActions();
+                            self._initCompleteFunctionality();
+                        } else if (new Date() - startDate > self.INIT_TIMEOUT) {
+                            clearInterval(_t);
+                            Unity.Launcher.removeActions();
+                            Unity.Notification.showNotification(self.WEBAPP_TITLE, "Failed to initialize, sorry :(", null);
+                        }
+                    }, 1000);
                 }
-            }), 500);
-        })
-    });
+            });
+        },
 
-    function newTab(link) {
-        return function() {
-            window.open(link);
+        _initCompleteFunctionality: function() {
+            var Unity = this.Unity;
+            var self = this;
+
+            window.Grooveshark.setSongStatusCallback(
+                /*
+                 * @param song {Object|null} Song record:
+                 *
+                 *     {
+                 *         songID: int,
+                 *         songName: String,
+                 *         artistID: int,
+                 *         artistName: String,
+                 *         albumID: int,
+                 *         albumName: String,
+                 *         trackNum: int,
+                 *         estimateDuration: Number (in milliseconds),
+                 *         artURL: String,
+                 *         calculatedDuration: Number (in milliseconds),
+                 *         position: Number (in milliseconds),
+                 *         vote: int (1 = Smile, -1 = Frown, 0 = No vote)
+                 *    }
+                 *
+                 * @param status {String} Status name (one of: "none", "loading", "playing", "paused", "buffering", "failed", "completed")
+                 */
+                function(res) {
+                    var song = res.song,
+                        status = res.status;
+
+                    Unity.Launcher.removeActions();
+
+                    switch (status) {
+                        case 'loading':
+                            Unity.Launcher.addAction(self.LOADING_SONG_ACTION, function() {});
+                            break;
+                        case 'playing':
+                            Unity.Launcher.addAction(self.PAUSE_ACTION, self.pause);
+                            clearTimeout(self._notificationTimeout);
+                            self._notificationTimeout = setTimeout(function() {
+                                Unity.Notification.showNotification(
+                                    song.songName,
+                                    "by " + song.artistName + " on " + song.albumName,
+                                    song.artURL
+                                );
+                            }, 500);
+                            break;
+                        case 'paused':
+                            Unity.Launcher.addAction(self.PLAY_ACTION, self.play);
+                            break;
+                    }
+
+                    Unity.Launcher.addAction(self.COLLECT_SONG_ACTION, self.collect);
+                    Unity.Launcher.addAction(self.FAVOURITE_SONG_ACTION, self.favourite);
+//                    Unity.Launcher.addAction(self.DEQUEUE_SONG_ACTION, self.dequeue);
+//                    if (window.Grooveshark.getPreviousSong())
+                    Unity.Launcher.addAction(self.PREVIOUS_SONG_ACTION, self.previous);
+//                    if (window.Grooveshark.getNextSong())
+                    Unity.Launcher.addAction(self.NEXT_SONG_ACTION, self.next);
+                }
+            );
+        },
+
+        newTab: function(link) {
+            return function() {
+                window.open(link);
+            }
+        },
+
+        play: function() {
+            window.Grooveshark.play();
+        },
+
+        pause: function() {
+            window.Grooveshark.pause();
+        },
+
+        next: function() {
+            window.Grooveshark.next();
+        },
+
+        previous: function() {
+            window.Grooveshark.previous();
+        },
+
+        collect: function() {
+            window.Grooveshark.addCurrentSongToLibrary();
+        },
+
+        favourite: function() {
+            window.Grooveshark.favoriteCurrentSong();
+        },
+
+        dequeue: function() {
+            window.Grooveshark.removeCurrentSongFromQueue();
         }
-    }
+    };
 
-    function play() {
-        return Grooveshark.play();
-    }
+    GroovesharkIntegration.init();
+}
 
-    function pause() {
-        return Grooveshark.pause();
-    }
+function evalInPageContext(func) {
+    var script = document.createElement('script');
+    script.appendChild(document.createTextNode('(' + func.toSource() + ')();'));
+    (document.body || document.head || document.documentElement).appendChild(script);
+}
 
-    function next() {
-        return Grooveshark.next();
-    }
-
-    function previous() {
-        return Grooveshark.previous();
-    }
-})();
+evalInPageContext(_initGroovesharkWebApp);
